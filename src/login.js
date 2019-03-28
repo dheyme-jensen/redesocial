d7cfd993742450dacdd8bc6433408f99ae7191d9
carlaam <carla.am@outlook.com>
2019-03-28 14:58:24 -0300

    Habilitada a conexão via facebook e Gmail

--- src/login.js
+++ src/login.js
@@ -1,43 +1,15 @@
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        document.getElementById('logged').style.display = 'block';
-        document.querySelector('.botao').style.display = 'block';
-        document.getElementById('logged').innerHTML = `You are current logged in ${user.value}`
-    }
-    else {
-        document.getElementById('logged').style.display = 'hide';
-        document.querySelector('.botao').style.display = 'hide';
-    }
-})
-
-/* document.querySelector('.submit-button').addEventListener('click', function () {
-    let userEmail = document.querySelector('.email').value;
-    let password = document.querySelector('.password').value;
-    firebase.auth().signInWithEmailAndPassword(userEmail, password).catch(function (error) {
-        window.alert('Error: ' + error.code);
-    })
-}) */
-
- $('.submit-button').click(function () {
+$('.submit-button').click(function () {
     let userEmail = document.querySelector('.email').value;
     let password = document.querySelector('.password').value;
 
     firebase.auth().signInWithEmailAndPassword(userEmail, password)
-    .then(function(response) {
-        window.location = "feed.html?id=" + response.user.uid;
-    })
-    .catch(function (error) {
-        window.alert('Error: ' + error.code);
-    })
+        .then(function (response) {
+            window.location = "feed.html?id=" + response.user.uid;
+        })
+        .catch(function (error) {
+            window.alert('Error: ' + error.code);
+        })
 })
- 
-/* document.querySelector('.botao').addEventListener('click', function(){
-    firebase.auth().signOut().then(function(){
-        window.alert('SignOut sucessfull!')
-    }), function (error){
-        window.alert(error.code);
-    }
-}) */
 
 $('.botao').click(function () {
     firebase.auth().signOut().then(function () {
@@ -47,4 +19,44 @@ $('.botao').click(function () {
     }), function (error) {
         window.alert(error.code);
     }
+})
+
+$('.google-auth').click(function () {
+    base_provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(base_provider).then(function (response) {
+        window.location = "feed.html?id=" + response.user.uid;
+        console.log('Success')
+    }).catch(function (error) {
+        console.log(error)
+    })
+})
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '{your-app-id}',
+        cookie: true,
+        xfbml: true,
+        version: '{api-version}'
+    });
+
+    FB.AppEvents.logPageView();
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+$('.facebook-auth').click(function () {
+    base_provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(base_provider).then(function (response) {
+        window.location = "feed.html?id=" + response.user.uid;
+        console.log('Success')
+    }).catch(function (error) {
+        console.log(error)
+    })
 })
