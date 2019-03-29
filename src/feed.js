d263fd8e5d82144b6dc1d491618ccd89ceff220d
alinemarjorie <aline.marjorie.af@gmail.com>
2019-03-29 13:18:43 -0300

    código refatorado

--- src/feed.js
+++ src/feed.js
@@ -1,88 +1,83 @@
 const database = firebase.database();
 let USER_ID = window.location.search.match(/\?id=(.*)/)[1];
 
-$(document).ready(function(){
-    
+$(document).ready(function(){    
+    getQuenstionsDB();
+    $('#form-post').submit(addQuestionsClick);
+    buttonPost();
+});
+
+function addQuestionsClick(e) {
+    e.preventDefault();    
+    $('#buttonPost').attr('disabled', true);
+
+    let newPost = $('.post').val(); 
+    let questionsFromDB = addQuestionsDB(newPost);
+
+    createPost(questionsFromDB.key, newPost);
+    deletePost(questionsFromDB.key);
+    editPost(questionsFromDB.key, newPost);
+};
+
+function addQuestionsDB(text) {
+    return database.ref(`questions/${USER_ID}`).push({
+        text: text
+    });
+};
+
+function buttonPost() {
+    $('.post').keyup(function(){
+        if ($('.post').val == '') {
+            $('#buttonPost').attr('disabled', true);
+            return;
+        }
+        $('#buttonPost').attr('disabled', false);
+    });
+};
+
+function getQuenstionsDB() {
     database.ref(`questions/${USER_ID}`).once('value')
         .then(function(snapshot){
             snapshot.forEach(function(childSnapshot){
                 let childKey = childSnapshot.key;
                 let childData = childSnapshot.val();
-                $('.timeline').prepend(`
-                    <div>
-                        <span data-text-id='${childKey}'>${childData.text}</span>                                               
-                        <button data-questions-id='${childKey}'> Excluir </button>
-                        <button data-edit-id='${childKey}'> Editar</button>
-                    </div>
-                `);               
-                
-                $(`button[data-questions-id='${childKey}']`).click(function(){
-                    let apagar = confirm('Deseja realmente excluir este registro?');
-                    if (apagar){
-                        database.ref(`questions/${USER_ID}/${childKey}`).remove();
-                        $(this).parent().remove();      		
-                    }else{
-                        event.preventDefault();
-                    }
-                });
-                    
-                $(`button[data-edit-id='${childKey}']`).click(function(){
-                    let newText =  prompt(`altere seu texto: ${childData.text}`);
-                    $(`span[data-text-id=${childKey}]`).text(newText);
-                    
-                    database.ref(`questions/${USER_ID}/${childKey}`).update({
-                        text: newText
-                    });
-                });        
+                createPost(childKey, childData.text);
+                deletePost(childKey);
+                editPost(childKey, childData.text);          
             });
         });
+};
 
-    $('#form-post').submit(function(e){
-        e.preventDefault();
-        $('#buttonPost').attr('disabled', true);
-
-        let newPost = $('.post').val(); 
-
-        let questionsFromDB = database.ref(`questions/${USER_ID}`).push({
-            text: newPost
-        });
-
-        $('.timeline').prepend(`
+function createPost(key, text) {
+    $('.timeline').prepend(`
             <div>
-                <span data-text-id='${questionsFromDB.key}'>${newPost}</span>
-                <button data-questions-id='${questionsFromDB.key}'> Excluir</button>
-                <button data-edit-id='${questionsFromDB.key}'> Editar</button>
+                <span data-text-id='${key}'>${text}</span>
+                <button data-questions-id='${key}'> Excluir</button>
+                <button data-edit-id='${key}'> Editar</button>
             </div>
-        `);
-      
-        $(`button[data-questions-id=${questionsFromDB.key}]`).click(function(event) {
-            let apagar = confirm('Deseja realmente excluir este registro?');
-            if (apagar){
-                database.ref(`questions/${USER_ID}/${questionsFromDB.key}`).remove();
-                $(this).parent().remove();      		
-            }else{
-                event.preventDefault();
-            }	
-        });
-    
-        $(`button[data-edit-id='${questionsFromDB.key}']`).click(function(){
-           let newText =  prompt(`altere seu texto: ${newPost}`);
-           $(`span[data-text-id=${questionsFromDB.key}]`).text(newText);
-           database.ref(`questions/${questionsFromDB.key}`).update({
-               newPost: newText
-           });
-           
-        });     
+        `)
+    $('.post').val('');
+};  
 
-        $('.post').val('');
+function deletePost(key){
+    $(`button[data-questions-id=${key}]`).click(function(event) {
+        let apagar = confirm('Deseja realmente excluir este registro?');
+        if (apagar){
+            database.ref(`questions/${USER_ID}/${key}`).remove();
+            $(this).parent().remove();      		
+        }else{
+            event.preventDefault();
+        }
     });
+};
 
-    $('.post').keyup(function(){
-    if ($('.post').val == '') {
-        $('#buttonPost').attr('disabled', true);
-        return;
-    }
-    $('#buttonPost').attr('disabled', false);
+function editPost(key, text){
+    $(`button[data-edit-id='${key}']`).click(function(){
+        let newText =  prompt(`altere seu texto: ${text}`);
+        $(`span[data-text-id=${key}]`).text(newText);
+            database.ref(`questions/${USER_ID}/${key}`).update({
+            text: newText
+        });           
     });
-});
+};
 
